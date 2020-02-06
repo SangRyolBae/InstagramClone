a55cd8e2dbbd12175c30bae5f933e9c1bca94a6b
BaeSangRyol <furyterrain@gmail.com>
2020-02-06 14:00:57 +0900

    Project Initialize Getting_Started_With_Firebase - 3. Log in User Interface PT. 2 END

---
+++ InstagramClone/Controller/LoginVC.swift
@@ -0,0 +1,102 @@
+//
+//  LoginVC.swift
+//  InstagramClone
+//
+//  Created by 배상렬 on 06/02/2020.
+//  Copyright © 2020 BaeSangRyol. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController
+{
+    let logoContainerView :UIView = {
+        let view = UIView();
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"));
+        logoImageView.contentMode = .scaleAspectFit;
+        view.addSubview(logoImageView);
+        
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50);
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true;
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true;
+        view.backgroundColor = UIColor(displayP3Red: 0, green: 120/255, blue: 175/255, alpha: 1);
+        return view;
+    }();
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField();
+        tf.placeholder = "Email";
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03);
+        tf.borderStyle = .roundedRect;
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf;
+    }();
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField();
+        tf.placeholder = "Password";
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03);
+        tf.borderStyle = .roundedRect;
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf;
+    }();
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system);
+        button.setTitle("Login", for: .normal);
+        button.setTitleColor(.white, for: .normal);
+        button.backgroundColor = UIColor(displayP3Red: 149/255, green: 204/255, blue: 244/255, alpha: 1);
+        button.layer.cornerRadius = 5;
+        return button;
+    }();
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system);
+        
+        let attributesTitle =  [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray];
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?    ", attributes: attributesTitle );
+        
+        let attributesSignUp = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)];
+        let attributedSignUp = NSAttributedString(string: "Sign Up", attributes: attributesSignUp );
+        
+        attributedTitle.append(attributedSignUp);
+        
+        button.setAttributedTitle(attributedTitle, for: .normal);
+        
+        return button;
+        
+    }();
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        //background color
+        view.backgroundColor = .white;
+        
+        print("View did load!!");
+        
+        view.addSubview(logoContainerView);
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150);
+        
+        configureViewComponents();
+        
+        view.addSubview(dontHaveAccountButton);
+        dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50);
+    }
+    
+    //
+    // View Component 셋팅
+    func configureViewComponents()
+    {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton]);
+        
+        stackView.axis = .vertical;
+        stackView.spacing = 10;
+        stackView.distribution = .fillEqually;
+        
+        view.addSubview(stackView);
+        stackView.anchor(top: logoContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140);
+        
+    }
+}
