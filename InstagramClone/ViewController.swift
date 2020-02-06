b00b245fa507127ef210d3018ce4651a4ba50ae6
BaeSangRyol <furyterrain@gmail.com>
2020-02-06 10:42:49 +0900

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by 배상렬 on 06/02/2020.
+//  Copyright © 2020 BaeSangRyol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
