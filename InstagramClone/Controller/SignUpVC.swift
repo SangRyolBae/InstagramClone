60ff084d9eed8f172b516251343db23ab4841eaa
배상렬 <furyterrain@blockbase.co.kr>
2020-02-06 20:16:24 +0900

    Firebase 연결 및 회원 가입

--- InstagramClone/Controller/SignUpVC.swift
+++ InstagramClone/Controller/SignUpVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpVC: UIViewController
 {
@@ -25,15 +26,7 @@ class SignUpVC: UIViewController
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03);
         tf.borderStyle = .roundedRect;
         tf.font = UIFont.systemFont(ofSize: 14)
-        return tf;
-    }();
-    
-    let passwordTextField: UITextField = {
-        let tf = UITextField();
-        tf.placeholder = "Password";
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03);
-        tf.borderStyle = .roundedRect;
-        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(formValidation), for:.editingChanged);
         return tf;
     }();
     
@@ -55,12 +48,26 @@ class SignUpVC: UIViewController
         return tf;
     }();
     
+    let passwordTextField: UITextField = {
+        let tf = UITextField();
+        tf.placeholder = "Password";
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03);
+        tf.borderStyle = .roundedRect;
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isSecureTextEntry = true;
+        tf.addTarget(self, action: #selector(formValidation), for:.editingChanged);
+        return tf;
+    }();
+    
     let signUpButton: UIButton = {
         let button = UIButton(type: .system);
         button.setTitle("Sign Up", for: .normal);
         button.setTitleColor(.white, for: .normal);
         button.backgroundColor = UIColor(displayP3Red: 149/255, green: 204/255, blue: 244/255, alpha: 1);
         button.layer.cornerRadius = 5;
+        button.isEnabled = false;
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside);
+        
         return button;
     }();
     
@@ -102,7 +109,7 @@ class SignUpVC: UIViewController
     
     func configureViewComponents()
     {
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, fullNameTextField, usernameTextField, signUpButton]);
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, fullNameTextField, usernameTextField, passwordTextField, signUpButton]);
         
         stackView.axis = .vertical;
         stackView.spacing = 10;
@@ -121,5 +128,50 @@ class SignUpVC: UIViewController
         
         navigationController?.popViewController(animated: true);
         
+    }
+    
+    @objc
+    func handleSignUp()
+    {
+    
+        print("Sign Up ");
+        
+        guard let email = emailTextField.text else {return};
+        guard let password = passwordTextField.text else {return};
+        
+        print("Email is \(email) and password is \(password)");
+        
+        Auth.auth().createUser(withEmail: email, password: password) {(user, error) in
+            
+            // handle error
+            if let error = error {
+                print("Failed to create user with error: " , error.localizedDescription);
+                return;
+            }
+            
+            // success
+            print("Successfully created user with Firebase");
+            
+            
+        }
+        //Auth.auth().createUser(withEmail)
+        
+    }
+    
+    @objc
+    func formValidation()
+    {
+        guard
+            emailTextField.hasText,
+            passwordTextField.hasText else {
+                
+                signUpButton.isEnabled = false;
+                signUpButton.backgroundColor = UIColor(displayP3Red: 149/255, green: 204/255, blue: 244/255, alpha: 1);
+                
+                return;
+        };
+        
+        signUpButton.isEnabled = true;
+        signUpButton.backgroundColor = UIColor(displayP3Red: 17/255, green: 154/255, blue: 237/255, alpha: 1);
     }
 }
