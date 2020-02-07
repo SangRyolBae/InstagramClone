4aa75057502a98e6ceac9f6aeea19f78e7a59770
BaeSangRyol <furyterrain@gmail.com>
2020-02-07 14:37:09 +0900

    Controller - Folder 정리

--- InstagramClone/SceneDelegate.swift
+++ InstagramClone/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             //window.rootViewController = LoginVC()
-            window.rootViewController = UINavigationController(rootViewController: LoginVC());
+            window.rootViewController = MainTabVC();//UINavigationController(rootViewController: LoginVC());
             self.window = window
             window.makeKeyAndVisible()
         }
