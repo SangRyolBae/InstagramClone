a72453e323e0888baecfe548142fdb992c12d17b
BaeSangRyol <furyterrain@gmail.com>
2020-02-17 17:46:12 +0900

    Notification : Cell 작업

---
+++ InstagramClone/View/NotificationCell.swift
@@ -0,0 +1,93 @@
+//
+//  NotificationCell.swift
+//  InstagramClone
+//
+//  Created by 배상렬 on 2020/02/17.
+//  Copyright © 2020 BaeSangRyol. All rights reserved.
+//
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - Properties
+       
+    let profileImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    let notificationLabel: UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "Username", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 12)])
+        attributedText.append(NSAttributedString(string: " Notification Comment", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]))
+        attributedText.append(NSAttributedString(string: " 2d", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor: UIColor.lightGray]))
+        label.attributedText = attributedText
+        label.numberOfLines = 2
+        label.textAlignment = .left;
+        return label
+    }()
+    
+    lazy var followButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)
+        button.addTarget(self, action: #selector(handleFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var postImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        
+        return iv
+    }()
+    
+    
+    // MARK: - handlers
+    @objc
+    func handleFollowTapped()
+    {
+        
+    }
+   
+    
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: nil, left: nil, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 40, height: 40)
+        postImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        
+        addSubview(notificationLabel)
+        notificationLabel.anchor(top: nil, left: profileImageView.rightAnchor, bottom: nil, right: postImageView.leftAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        notificationLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        addSubview(followButton);
+        followButton.anchor(top: nil, left: nil, bottom: nil, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 90, height: 30);
+        followButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true;
+        followButton.layer.cornerRadius = 3;
+        followButton.isHidden = true;
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
