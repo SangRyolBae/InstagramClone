4d92d548f3f97bf0d86d1fc51346e6d3434ecc78
BaeSangRyol <furyterrain@gmail.com>
2020-02-20 16:20:56 +0900

    Feed 페이징 작업

--- InstagramClone/Controller/Feed/FeedVC.swift
+++ InstagramClone/Controller/Feed/FeedVC.swift
@@ -19,6 +19,7 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
     var posts = [Post]();
     var viewSinglePost = false;
     var post:Post?;
+    var currentKey: String?
     
     override func viewDidLoad()
     {
@@ -59,7 +60,17 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
     
     
     // MARK: - UICollectionViewDataSource
-
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+    {
+        if posts.count > 4
+        {
+            if indexPath.item == posts.count - 1
+            {
+                fetchPosts();
+            }
+        }
+    }
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -124,24 +135,56 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
     {
         guard let currentUid = Auth.auth().currentUser?.uid else {return};
         
-        USER_FEED_REF.child(currentUid).observe(.childAdded) { (snapshot) in
-            
-            let postId = snapshot.key;
-            
-            Database.fetchPost(with: postId) { (post) in
-                    
-                self.posts.append(post);
+        if nil == currentKey
+        {
+            USER_FEED_REF.child(currentUid).queryLimited(toLast: 5).observeSingleEvent(of: .value) { (snapshot) in
                 
-                self.posts.sort { (post1, post2) -> Bool in
-                    return post1.creationDate > post2.creationDate;
-                }
-                
-                //stop refreshing;
                 self.collectionView?.refreshControl?.endRefreshing();
                 
-                self.collectionView?.reloadData();
+                guard let first = snapshot.children.allObjects.first as? DataSnapshot else {return };
+                guard let allObjects = snapshot.children.allObjects as? [DataSnapshot] else {return};
                 
+                allObjects.forEach { (snapshot) in
+                    
+                    let postId = snapshot.key
+                    
+                    self.fetchPost(withPostId: postId);
+                }
+                
+                self.currentKey = first.key;
             }
+        }else
+        {
+            USER_FEED_REF.child(currentUid).queryOrderedByKey().queryEnding(atValue: self.currentKey).queryLimited(toLast: 6).observeSingleEvent(of: .value) { (snapshot) in
+                
+                guard let first = snapshot.children.allObjects.first as? DataSnapshot else {return };
+                guard let allObjects = snapshot.children.allObjects as? [DataSnapshot] else {return};
+                
+                allObjects.forEach { (snapshot) in
+                    
+                    let postId = snapshot.key
+                    
+                    if postId != self.currentKey {
+                        self.fetchPost(withPostId: postId);
+                    }
+                    
+                }
+                
+                self.currentKey = first.key;
+            }
+        }
+    }
+    
+    func fetchPost(withPostId postId:String)
+    {
+        Database.fetchPost(with: postId) { (post) in
+            self.posts.append(post);
+
+            self.posts.sort { (post1, post2) -> Bool in
+                return post1.creationDate > post2.creationDate;
+            }
+
+            self.collectionView?.reloadData();
         }
     }
     
